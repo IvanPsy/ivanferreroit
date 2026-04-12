1cbcb702c8b04fad144c14b37514adcc6b406618
IvanPsy <ivan@ivanferrero.it>
2026-04-12 21:30:38 +0200

    fixed

--- assets/menu.js
+++ assets/menu.js
@@ -22,6 +22,7 @@ const menuConfig = {
     items: [
         { label: 'Home', href: '/' },
         { label: 'Dashboard', href: '/dashboards/' },
+        { label: 'Digitale Inclusivo', href: '/digitale-inclusivo/decodificatore-icf/' },
         { label: 'Dialoghi', href: '/dialoghi/' },
         { label: 'Chi Sono', href: '/#chi-sono' },
         { label: 'Contatti', href: '/#contatti' }
